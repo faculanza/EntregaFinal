103bf72578be14cd7d8183fda61101143fdc61e1
faculanza <31344783+faculanza@users.noreply.github.com>
2022-12-11 11:46:31 -0300

    Add files via upload

---
+++ AppFinal/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from AppFinal import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns =[
+    path('',views.inicio, name='inicio'),
+    path('about/',views.about, name='about'),  
+    path('registro/',views.registro, name='registro'),  
+    path('login/',views.login, name='login'),  
+    path('chat/',views.chat, name='chat'),
+    # path('AgregarCategoria/',views.AgregarCategoria, name='AgregarCategoria'),
+    # path('AgregarProducto/',views.AgregarProducto, name='AgregarProducto'),
+    # path('buscarproducto/',views.buscarproducto, name='buscarproducto'),
+    # path('resultado_producto/',views.resultado_producto, name='resultado_producto'),
+
+    
+
+
+]
--- AppFinal/views.py
+++ AppFinal/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def registro(request):
+    return render(request, "registro.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def chat(request):
+    return render(request, "chat.html")
+
